bd7c878a57bc2a22afcd7e3b4d793ae191102207
Leticia Tran <leticiastran@gmail.com>
2018-03-11 17:23:51 -0700

    Refactoring

--- lib/block.rb
+++ lib/block.rb
@@ -1,3 +1,4 @@
+require 'awesome_print'
 module Hotel
   class Block
     attr_reader :id, :date_range, :rooms, :discount_rate
@@ -14,11 +15,11 @@ module Hotel
 
     def available_rooms
       available_rooms = rooms
-      # puts "available_rooms = #{available_rooms}"
-      #
+
       date_range.each do |date|
-        rooms.each {|room| available_rooms.delete(room) if room.ocupied_on.include?(date)}
+        available_rooms.delete_if {|room| room.ocupied_on.include?(date) }
       end
+
 
       return available_rooms
     end
