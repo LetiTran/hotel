35ef9d46d2f638c633ad3ff9523c89385ad034bf
Leticia Tran <leticiastran@gmail.com>
2018-04-01 11:35:14 -0700

    Deleted usage of available_rooms method and got Manager to be responsible for it.

--- lib/block.rb
+++ lib/block.rb
@@ -1,6 +1,6 @@
-require 'awesome_print'
+require_relative 'manager'
 module Hotel
-  class Block
+  class Block < Manager
     attr_reader :id, :date_range, :rooms, :discount_rate
 
     def initialize(id:, date_range:, rooms:, discount_rate:)
@@ -10,18 +10,30 @@ module Hotel
       @id = id
       @date_range = date_range
       @rooms = rooms
+      # @available_rooms = rooms
+      # @reserved_rooms = []
       @discount_rate = discount_rate
     end
 
-    def available_rooms
-      available_rooms = rooms
-
-      date_range.each do |date|
-        available_rooms.delete_if {|room| room.ocupied_on.include?(date) }
-      end
-
-
-      return available_rooms
-    end
+    # def available_rooms
+    #   available_rooms = rooms
+    #   # TODO: DRY
+    #   date_range.each do |date|
+    #     # available_rooms.delete_if {|room| room.ocupied_on.include?(date) }
+    #     available_rooms.each do |room|
+    #       Manager.all_reservations.each do |reservation|
+    #         all_res.each do |reservation|
+    #           if reservation.room == room
+    #             if  get_date_range(reservation.start_date, reservation.end_date).include?(date)
+    #               available_rooms.delete(room)
+    #             end
+    #           end
+    #         end
+    #       end
+    #     end
+    #
+    #     return available_rooms
+    #   end
+    # end
   end
 end
